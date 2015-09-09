a47e9657f6579938b0fa0f8ef81f710b89070ed4
kbanner14 <katharine.banner@gmail.com>
2015-09-09 14:12:49 -0600

    more work on vignette and added a data file

---
+++ MApp/R/brain_ex_data.R
@@ -0,0 +1,16 @@
+#' Brain weight, body weight, gestation length and litter size
+#'
+#' A dataset containing the log brain weight log(g), log body weight log(kg),
+#' log gestation lenght log(days), and litter size (# offspring) for 96 species
+#' of mammals. These data were modified from the \code{case0}
+#' @format A data frame with 96 rows and 4 variables: \describe{
+#'   \item{lbrain}{log transformed average brain weight, in log(grams)}
+#'   \item{lbody}{log transformed average body weight, in log(kg)}
+#'   \item{lgest}{log transformed average gestation length, log(days)}
+#'   \item{litter}{average number of offspring in one litter (# offspring)} }
+#' @source Original by F.L. Ramsey and D.W. Schafer and modifications by Daniel
+#'   W. Schafer and Jeannie Sifneos and Berwin A. Turlach, (2012). Sleuth2: Data
+#'   sets from Ramsey and Schafer's "Statistical Sleuth (2nd ed)",
+#'   \url{http://CRAN.R-project.org/package=Sleuth2}
+#'
+"brainData"
